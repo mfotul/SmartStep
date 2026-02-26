e652e375341605444ff34c8df712603521a9f805
Marian Fotul <marian.fotul@swan.sk>
2026-02-26 15:13:42 +0100

    Room added, weekly stats implemented, profile optimized

--- app/src/main/java/com/example/smartstep/core/presentation/designsystem/pickerdialogs/PickerColumn.kt
+++ app/src/main/java/com/example/smartstep/core/presentation/designsystem/pickerdialogs/PickerColumn.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.smartstep.R
 import com.example.smartstep.core.presentation.designsystem.theme.SmartStepTheme
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.filter
