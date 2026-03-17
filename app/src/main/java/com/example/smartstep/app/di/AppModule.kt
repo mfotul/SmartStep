f5ce41b55166b5ed08c0bb881a2fb9c3920a1af8
Marian Fotul <marian.fotul@swan.sk>
2026-03-17 11:56:09 +0100

    Milestone #3

--- app/src/main/java/com/example/smartstep/app/di/AppModule.kt
+++ app/src/main/java/com/example/smartstep/app/di/AppModule.kt
@@ -1,12 +1,5 @@
 package com.example.smartstep.app.di
 
-import ai.koog.agents.core.agent.AIAgent
-import ai.koog.agents.core.tools.ToolRegistry
-import ai.koog.agents.core.tools.ToolRegistry.Companion.invoke
-import ai.koog.agents.core.tools.reflect.tool
-import ai.koog.prompt.executor.clients.google.GoogleModels
-import ai.koog.prompt.executor.llms.all.simpleGoogleAIExecutor
-import com.example.smartstep.BuildConfig
 import com.example.smartstep.app.MainViewModel
 import com.example.smartstep.app.SmartStepApplication
 import kotlinx.coroutines.CoroutineScope
--- app/src/main/java/com/example/smartstep/smart/presentation/step/StepScreen.kt
+++ app/src/main/java/com/example/smartstep/smart/presentation/step/StepScreen.kt
@@ -18,9 +18,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
